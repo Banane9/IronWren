6ca311b8c39f54aed638b38f47a200ad52549442
Mia Boulter <miaboulter@outlook.com>
2025-06-18 22:29:24 +0100

    Normal class tests

--- IronWren.Tests/FullyAutoMapper.cs
+++ IronWren.Tests/FullyAutoMapper.cs
@@ -16,6 +16,25 @@ public class FullyAutoMapperTests
     }
 
     [TestMethod]
+    public void NormalClass()
+    {
+        vm.FullyAutoMap<Vector>();
+
+        Assert.AreEqual(WrenInterpretResult.Success, vm.Interpret("var vec = Vector.new(1, 2)\nSystem.print(\"Vector (%(vec.x), %(vec.y))\")"));
+        Assert.AreEqual("Vector (1, 2)", output[0]);
+        output.Clear();
+
+        Assert.AreEqual(WrenInterpretResult.Success, vm.Interpret("vec.add(Vector.new(3, 4))\nSystem.print(\"Vector (%(vec.x), %(vec.y))\")"));
+        Assert.AreEqual("Vector (4, 6)", output[0]);
+        output.Clear();
+
+        Assert.AreEqual(WrenInterpretResult.Success, vm.Interpret("var length = Vector.getLength(Vector.new(0, 2))"));
+        vm.EnsureSlots(1);
+        vm.GetVariable(WrenVM.MainModule, "length", 0);
+        Assert.AreEqual(2, vm.GetSlotDouble(0));
+    }
+
+    [TestMethod]
     public void StaticClassInModule()
     {
         vm.FullyAutoMap("math", typeof(Math));
@@ -37,6 +56,30 @@ public class FullyAutoMapperTests
         public static double Sin(double value)
         {
             return System.Math.Sin(value);
+        }
+    }
+
+    private class Vector
+    {
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public static double GetLength(Vector vec)
+        {
+            return System.Math.Sqrt((vec.X * vec.X) + (vec.Y * vec.Y));
+        }
+
+        public void Add(Vector other)
+        {
+            X += other.X;
+            Y += other.Y;
         }
     }
 }
